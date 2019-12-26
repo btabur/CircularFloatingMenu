0ef7710f76b509dacbc24e88a7a389582dc3e221
btabur <56840174+btabur@users.noreply.github.com>
2019-12-26 23:36:43 +0300

    gönderme

---
+++ app/src/main/java/com/example/deneme_circular_floating_action_menu/MainActivity.java
@@ -0,0 +1,178 @@
+package com.example.deneme_circular_floating_action_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        //Sağ alt köşede bir floatin buton oluşturduk....
+        final ImageView fabIconNew = new ImageView(this);
+        fabIconNew.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_new_light));
+        final FloatingActionButton rightLowerButton = new FloatingActionButton.Builder(this)
+                .setContentView(fabIconNew)
+                .build();
+
+        //-------------Floatin butona basıldığında oluşacak butonları oluşturuyoruz.
+
+        SubActionButton.Builder rLSubBuilder = new SubActionButton.Builder(this);
+        ImageView rlIcon1 = new ImageView(this);
+        ImageView rlIcon2 = new ImageView(this);
+        ImageView rlIcon3 = new ImageView(this);
+        ImageView rlIcon4 = new ImageView(this);
+
+        rlIcon1.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_chat_light));
+        rlIcon2.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_camera_light));
+        rlIcon3.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_video_light));
+        rlIcon4.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_place_light));
+
+
+
+        //-----------------4 buton tane butonu floating butona ekliyoruz
+
+        final FloatingActionMenu rightLowerMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(rLSubBuilder.setContentView(rlIcon1).build())
+                .addSubActionView(rLSubBuilder.setContentView(rlIcon2).build())
+                .addSubActionView(rLSubBuilder.setContentView(rlIcon3).build())
+                .addSubActionView(rLSubBuilder.setContentView(rlIcon4).build())
+                .attachTo(rightLowerButton)
+                .build();
+
+        //--------------menu açıldığında ve kapandığında ki olayları dinliyoruz
+        rightLowerMenu.setStateChangeListener(new FloatingActionMenu.MenuStateChangeListener() {
+            @Override
+            public void onMenuOpened(FloatingActionMenu menu) {
+                // floatin buton açıldığında animasyonlu bir şekilde 45 derece dönmesini sağlıyoruz.
+
+                fabIconNew.setRotation(0);
+                PropertyValuesHolder pvhR = PropertyValuesHolder.ofFloat(View.ROTATION, 45);
+                ObjectAnimator animation = ObjectAnimator.ofPropertyValuesHolder(fabIconNew, pvhR);
+                animation.start();
+            }
+
+            @Override
+            public void onMenuClosed(FloatingActionMenu menu) {
+                // Kapandığı zaman 45 derece geri dönmesini sağlıyoruz....
+                fabIconNew.setRotation(45);
+                PropertyValuesHolder pvhR = PropertyValuesHolder.ofFloat(View.ROTATION, 0);
+                ObjectAnimator animation = ObjectAnimator.ofPropertyValuesHolder(fabIconNew, pvhR);
+                animation.start();
+            }
+        });
+
+
+        //-----------------orta sağdaki büyük kırmızı butonu oluşturuyoruz.
+
+        int redActionButtonSize = getResources().getDimensionPixelSize(R.dimen.red_action_button_size);
+        int redActionButtonMargin = getResources().getDimensionPixelOffset(R.dimen.action_button_margin);
+        int redActionButtonContentSize = getResources().getDimensionPixelSize(R.dimen.red_action_button_content_size);
+        int redActionButtonContentMargin = getResources().getDimensionPixelSize(R.dimen.red_action_button_content_margin);
+        int redActionMenuRadius = getResources().getDimensionPixelSize(R.dimen.red_action_menu_radius);
+        int blueSubActionButtonSize = getResources().getDimensionPixelSize(R.dimen.blue_sub_action_button_size);
+        int blueSubActionButtonContentMargin = getResources().getDimensionPixelSize(R.dimen.blue_sub_action_button_content_margin);
+
+        ImageView fabIconStar = new ImageView(this);
+        fabIconStar.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_important));
+
+        // Kırmızı butona margin veriyoruz....
+        FloatingActionButton.LayoutParams starParams = new FloatingActionButton.LayoutParams(redActionButtonSize, redActionButtonSize);
+        starParams.setMargins(redActionButtonMargin,
+                redActionButtonMargin,
+                redActionButtonMargin,
+                redActionButtonMargin);
+        fabIconStar.setLayoutParams(starParams);
+
+        FloatingActionButton.LayoutParams fabIconStarParams = new FloatingActionButton.LayoutParams(redActionButtonContentSize, redActionButtonContentSize);
+        fabIconStarParams.setMargins(redActionButtonContentMargin,
+                redActionButtonContentMargin,
+                redActionButtonContentMargin,
+                redActionButtonContentMargin);
+
+        //Büyük kırmızı butonu oluşturduk.......
+
+        final FloatingActionButton leftCenterButton = new FloatingActionButton.Builder(this)
+                .setContentView(fabIconStar, fabIconStarParams)
+                .setBackgroundDrawable(R.drawable.button_action_red_selector)
+                .setPosition(FloatingActionButton.POSITION_LEFT_CENTER)
+                .setLayoutParams(starParams)
+                .build();
+
+        // küçük mavi butonların konteinerini  oluşturduk...
+        SubActionButton.Builder lCSubBuilder = new SubActionButton.Builder(this);
+        lCSubBuilder.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_action_blue_selector));
+
+        FrameLayout.LayoutParams blueContentParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+        blueContentParams.setMargins(blueSubActionButtonContentMargin,
+                blueSubActionButtonContentMargin,
+                blueSubActionButtonContentMargin,
+                blueSubActionButtonContentMargin);
+        lCSubBuilder.setLayoutParams(blueContentParams);
+
+
+        FrameLayout.LayoutParams blueParams = new FrameLayout.LayoutParams(blueSubActionButtonSize, blueSubActionButtonSize);
+        lCSubBuilder.setLayoutParams(blueParams);
+
+
+        //-----------ikonlarımızı oluşturduk--------
+        ImageView lcIcon1 = new ImageView(this);
+        ImageView lcIcon2 = new ImageView(this);
+        ImageView lcIcon3 = new ImageView(this);
+        ImageView lcIcon4 = new ImageView(this);
+        ImageView lcIcon5 = new ImageView(this);
+
+        lcIcon1.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_camera));
+        lcIcon2.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_picture));
+        lcIcon3.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_video));
+        lcIcon4.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_location_found));
+        lcIcon5.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_headphones));
+
+        // Build another menu with custom options
+        final FloatingActionMenu leftCenterMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(lCSubBuilder.setContentView(lcIcon1, blueContentParams).build())
+                .addSubActionView(lCSubBuilder.setContentView(lcIcon2, blueContentParams).build())
+                .addSubActionView(lCSubBuilder.setContentView(lcIcon3, blueContentParams).build())
+                .addSubActionView(lCSubBuilder.setContentView(lcIcon4, blueContentParams).build())
+                .addSubActionView(lCSubBuilder.setContentView(lcIcon5, blueContentParams).build())
+                .setRadius(redActionMenuRadius)
+                .setStartAngle(70)
+                .setEndAngle(-70)
+                .attachTo(leftCenterButton)
+                .build();
+
+
+        leftCenterMenu.setStateChangeListener(new FloatingActionMenu.MenuStateChangeListener() {
+            @Override
+            public void onMenuOpened(FloatingActionMenu floatingActionMenu) {
+
+            }
+
+            @Override
+            public void onMenuClosed(FloatingActionMenu floatingActionMenu) {
+
+            }
+        });
+
+    }
+
+
+}
